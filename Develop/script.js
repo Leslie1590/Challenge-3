66c49e33bc8622a6d7e76bbe976bcf63eb74eab0
Crayen <transparent_legacy@yahoo.com>
2022-07-28 15:41:18 -0500

    added prompt and log for length

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,43 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+  
+
+// Start of password input function
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    var password = generatePassword();
 
-}
+    var passwordText = document.querySelector("#password");
+  
+    // Makes the password show on screen
+    passwordText.value = password;
+
+
+    //generate password function
+    function generatePassword() {
+      var passwordLength = "";
+
+      while (passwordLength === "" || passwordLength === null) {
+      passwordLength = prompt("What length is your password? TYPE any number between 8 and 128.")
+        }
+        console.log(passwordLength);
+      if (passwordLength <=7) {
+        window.alert ("Please enter in a correct value.")
+      }
+      else if (passwordLength >=129) {
+        window.alert ("Please enter in a correct value.")
+      }
+      else {
+        window.alert ("You have chosen to have " + passwordLength + " characters.")
+      }
+    }
+};
+
+
+
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
