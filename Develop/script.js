254a5920a40fbb77a424ceee11b9685f9e98336a
Crayen <transparent_legacy@yahoo.com>
2022-07-31 13:58:59 -0500

    added character and special prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,15 @@
-// Get references to the #generate element
+// get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 
   
 
-// Start of password input function
+// start of password input function
 function writePassword() {
 
     var password = generatePassword();
+    var password = generateCharacters ();
+    var password = generateSpecial ();
 
     var passwordText = document.querySelector("#password");
   
@@ -15,14 +17,13 @@ function writePassword() {
     passwordText.value = password;
 
 
-    //generate password function
+    // generate password function
     function generatePassword() {
       var passwordLength = "";
 
       while (passwordLength === "" || passwordLength === null) {
       passwordLength = prompt("What length is your password? TYPE any number between 8 and 128.")
         }
-        console.log(passwordLength);
       if (passwordLength <=7) {
         window.alert ("Please enter in a correct value.")
       }
@@ -32,8 +33,52 @@ function writePassword() {
       else {
         window.alert ("You have chosen to have " + passwordLength + " characters.")
       }
+      console.log(passwordLength);
+      
     }
+
+     // generate character types
+    function generateCharacters() {
+    var passwordCharacters = "";
+
+    while (passwordCharacters === "" || passwordCharacters === null) {
+      passwordCharacters = prompt("Would you like to add UPPER case characters? Press 1 for YES or 2 for NO.")
+      }
+      if (passwordCharacters === "1") {
+      console.log(passwordCharacters)
+      }
+      else if (passwordCharacters === "2") {
+      console.log(passwordCharacters)
+      }
+      else {
+      window.alert ("Please choose a correct value.")
+      }
+    }
+
+    // generate special character types
+    function generateSpecial() {
+      var passwordSpecial = "";
+
+    while (passwordSpecial === "" || passwordSpecial === null) {
+      passwordSpecial = prompt("Would you like to add SPECIAL characters? Press 1 for YES and 2 for NO.")
+      }
+      if (passwordSpecial === "1") {
+      console.log(passwordSpecial)
+      }
+      else if (passwordSpecial === "2") {
+      console.log(passwordSpecial)
+      }
+      else {
+      window.alert ("Please choose a correct value.")
+      }
+    }
+
+
+
+
 };
+
+
 
 
 
