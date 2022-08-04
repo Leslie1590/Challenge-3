22c9afce00fd606db5c6736bb7b43772634cf675
Crayen <transparent_legacy@yahoo.com>
2022-08-03 19:12:14 -0500

    arrays made

--- script.js
+++ script.js
@@ -1,86 +1,85 @@
+var passwordCharacters = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+var passwordSpecial = [
+  "!", "@", "#", "$", "%",
+]
+
 // get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // generate password function
-  function generatePassword() { 
-  console.log("Hey")
-
+function generatePassword() { 
   
-  
+  //password length prompt
   var passwordLength = "";
 
-  while (passwordLength === "" || passwordLength === null) {
-  passwordLength = prompt("What length is your password? TYPE any number between 8 and 128.")
-  }
-  if (passwordLength <=7) {
-  window.alert ("Please enter in a correct value.")
-  }
-  else if (passwordLength >=129) {
-  window.alert ("Please enter in a correct value.")
-  }
-  else {
-  window.alert ("You have chosen to have " + passwordLength + " characters.")
-  }
-  return passwordLength 
+    while (passwordLength === "" || passwordLength === null) {
+      passwordLength = prompt("What length is your password? TYPE any number between 8 and 128.")
+    }
+      if (passwordLength <=7) {
+      window.alert ("Please enter in a correct value.")
+    }
+    else if (passwordLength >=129) {
+      window.alert ("Please enter in a correct value.")
+    }
+    else {
+      window.alert ("You have chosen to have " + passwordLength + " characters.")
+    }
 
-  // generate character types
-  function generateCharacters() {
-    var passwordCharacters = "";
+  //password characters prompt
+  var passwordCharacters = "";
 
     while (passwordCharacters === "" || passwordCharacters === null) {
       passwordCharacters = prompt("Would you like to add UPPER case characters? Press 1 for YES or 2 for NO.")
       }
+      // if 1 is true
       if (passwordCharacters === "1") {
       console.log(passwordCharacters)
       }
+      // if 2 is false
       else if (passwordCharacters === "2") {
-      console.log(passwordCharacters)
       }
       else {
       window.alert ("Please choose a correct value.")
       }
-    }
+      return passwordCharacters;
 
-    // generate special character types
-    function generateSpecial() {
-      var passwordSpecial = "";
+  // generate special character types
+  var passwordSpecial = "";
 
     while (passwordSpecial === "" || passwordSpecial === null) {
       passwordSpecial = prompt("Would you like to add SPECIAL characters? Press 1 for YES and 2 for NO.")
       }
+      // if 1 is true
       if (passwordSpecial === "1") {
       console.log(passwordSpecial)
       }
+      // if 2 is false
       else if (passwordSpecial === "2") {
-      console.log(passwordSpecial)
       }
       else {
       window.alert ("Please choose a correct value.")
-      }
     }
-  };
 
-// start of password input function
+    
+};
+
+
+
+// password input function
 function writePassword() {
     var password = generatePassword();
    
     var passwordText = document.querySelector("#password");
   
-    // Makes the password show on screen
+    // makes the password show on screen
     passwordText.value = password;
+
+    return passwordCharacters;
   };
 
-
-
-
-   
-
-
-
-
-
-
-// Add event listener to generate button
+// add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
 
